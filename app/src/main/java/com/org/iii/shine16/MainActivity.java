9d430aad1298fe8cbba24fc2c185ecd23126d15b
user <a>
2016-11-24 15:16:18 +0800

    v2 webView

--- app/src/main/java/com/org/iii/shine16/MainActivity.java
+++ app/src/main/java/com/org/iii/shine16/MainActivity.java
@@ -2,12 +2,23 @@ package com.org.iii.shine16;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webView);
+        initWebView();
+    }
+    private void initWebView(){
+        //1.
+        //webView.loadUrl("http://taichung.iiiedu.org.tw/");
+        //2.   //後面的/是根目錄
+        webView.loadUrl("file:///android_asset/shine.html");
+
     }
 }
